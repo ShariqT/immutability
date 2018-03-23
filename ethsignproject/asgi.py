8b55b5ba85ae3c2c19488e70dc411d766873bb82
Shariq Torres <shariq.torres@gmail.com>
2018-03-22 23:12:30 -0700

    whitenoise

--- ethsignproject/asgi.py
+++ ethsignproject/asgi.py
@@ -6,7 +6,8 @@ defined in the ASGI_APPLICATION setting.
 import os
 import django
 from channels.routing import get_default_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ethsignproject.settings")
 django.setup()
-application = get_default_application()
+application = DjangoWhiteNoise(get_default_application())
